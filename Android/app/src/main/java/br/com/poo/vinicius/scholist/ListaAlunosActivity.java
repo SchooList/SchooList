ebb57bf2d1aa015cfccf1e98594e3c62e5eee3c1
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-01-04 23:11:30 -0300

    Added adapter

--- Android/app/src/main/java/br/com/poo/vinicius/scholist/ListaAlunosActivity.java
+++ Android/app/src/main/java/br/com/poo/vinicius/scholist/ListaAlunosActivity.java
@@ -2,6 +2,8 @@ package br.com.poo.vinicius.scholist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Daniel", "Felipe", "Vinicius"};
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(ListaAlunosActivity.this, android.R.layout.activity_list_item, alunos);
+        listaAlunos.setAdapter(adapter);
+
+
+
     }
 }
