fddbf88d4e641ae4baffdf61366e02f4f7f5d8bb
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-01-07 20:23:48 -0300

    Added listener to button save

--- Android/app/src/main/java/br/com/poo/vinicius/scholist/FormularioActivity.java
+++ Android/app/src/main/java/br/com/poo/vinicius/scholist/FormularioActivity.java
@@ -2,6 +2,9 @@ package br.com.poo.vinicius.scholist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormularioActivity.this,"Usuário Salvo Com Sucesso", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
     }
 }
