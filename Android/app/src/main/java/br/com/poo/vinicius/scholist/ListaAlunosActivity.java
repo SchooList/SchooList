37898052b7be9676ebb7546e92c240b99c8e6ad2
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-01-12 21:41:25 -0300

    Add new features to fixed later

--- Android/app/src/main/java/br/com/poo/vinicius/scholist/ListaAlunosActivity.java
+++ Android/app/src/main/java/br/com/poo/vinicius/scholist/ListaAlunosActivity.java
@@ -77,6 +77,17 @@ public class ListaAlunosActivity extends AppCompatActivity {
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
         final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
 
+        MenuItem itemSMS = menu.add("Enviar SMS");
+        Intent intentSMS = new Intent(Intent.ACTION_VIEW);
+        intentSMS.setData(Uri.parse("sms:" + aluno.getTelefone()));
+        itemSMS.setIntent(intentSMS);
+
+        MenuItem itemMapa = menu.add("Visualizar no mapa");
+        Intent intentMapa = new Intent(Intent.ACTION_VIEW);
+        intentMapa.setData(Uri.parse("geo:0,0?q=" + aluno.getEndereco()));
+        itemMapa.setIntent(intentMapa);
+
+
         MenuItem itemSite = menu.add("Visitar Site");
         Intent intentSite = new Intent(Intent.ACTION_VIEW);
         String site = aluno.getSite();
@@ -84,11 +95,6 @@ public class ListaAlunosActivity extends AppCompatActivity {
             site = "http://" + site;
         }
         intentSite.setData(Uri.parse(site));
-
-
-        MenuItem itemTelefone = menu.add("Ligar para: " + aluno.getNome());
-
-
 
         MenuItem delete = menu.add("Deletar");
         delete.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
