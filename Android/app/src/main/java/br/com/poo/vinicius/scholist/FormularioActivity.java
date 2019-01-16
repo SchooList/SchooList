83231ebf424dae73b3c0260d00488663047de34d
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-01-15 22:27:23 -0300

    Fixed bug in camera

--- Android/app/src/main/java/br/com/poo/vinicius/scholist/FormularioActivity.java
+++ Android/app/src/main/java/br/com/poo/vinicius/scholist/FormularioActivity.java
@@ -2,6 +2,9 @@ package br.com.poo.vinicius.scholist;
 
 import android.content.Intent;
 import android.media.Rating;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +14,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.File;
 
 import br.com.poo.vinicius.scholist.dao.AlunoDAO;
 import br.com.poo.vinicius.scholist.model.Aluno;
@@ -25,12 +30,28 @@ public class FormularioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
         helper = new FormularioHelper(this);
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
         Aluno aluno = (Aluno) intent.getSerializableExtra("aluno");
         helper = new FormularioHelper(this);
         if(aluno != null) {
             helper.preencheFormulario(aluno);
         }
+
+        Button botaoFoto = (Button) findViewById(R.id.formulario_botao_foto);
+
+        botaoFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                String caminhoFoto = getExternalFilesDir(null) + "/" + System.currentTimeMillis() + ".jpg";
+                File arquivoFoto = new File(caminhoFoto);
+                Uri fotoUri = FileProvider.getUriForFile(FormularioActivity.this, BuildConfig.APPLICATION_ID + ".provider", arquivoFoto);
+                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, fotoUri);
+                startActivity(intentCamera);
+            }
+        });
+
+
     }
 
     @Override
