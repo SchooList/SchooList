6728a76241c87d303488785a04ce452a87b1fc28
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-01-17 17:57:59 -0300

    Set adapter to adjust item from listview

--- Android/app/src/main/java/br/com/poo/vinicius/scholist/adapter/AlunosAdapter.java
+++ Android/app/src/main/java/br/com/poo/vinicius/scholist/adapter/AlunosAdapter.java
@@ -1,24 +1,28 @@
 package br.com.poo.vinicius.scholist.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
+import java.util.zip.Inflater;
 
 import br.com.poo.vinicius.scholist.ListaAlunosActivity;
+import br.com.poo.vinicius.scholist.R;
 import br.com.poo.vinicius.scholist.model.Aluno;
-
 public class AlunosAdapter extends BaseAdapter {
-
     private final List<Aluno> alunos;
     private final Context context;
 
     public AlunosAdapter(Context context, List<Aluno> alunos) {
-            this.context = context;
-            this.alunos = alunos;
+        this.context = context;
+        this.alunos = alunos;
     }
 
     @Override
@@ -28,19 +32,40 @@ public class AlunosAdapter extends BaseAdapter {
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return alunos.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return alunos.get(position).getId();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        Aluno aluno = alunos.get()
-        TextView view = new TextView(context);
-        view.setText(alunos.toString());
+        Aluno aluno = alunos.get(position);
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = convertView;
+        if (view == null) {
+            view = inflater.inflate(R.layout.list_item, parent, false);
+        }
+
+        TextView campoNome = (TextView) view.findViewById(R.id.item_nome);
+        campoNome.setText(aluno.getNome());
+
+        TextView campoTelefone = (TextView) view.findViewById(R.id.item_telefone);
+        campoTelefone.setText(aluno.getTelefone());
+
+        ImageView campoFoto = (ImageView) view.findViewById(R.id.item_foto);
+        String caminhoFoto = aluno.getCaminhoFoto();
+        if (caminhoFoto != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduce = Bitmap.createScaledBitmap(bitmap, 100, 100, true);
+            campoFoto.setImageBitmap(bitmapReduce);
+            campoFoto.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
+
         return view;
     }
 }
