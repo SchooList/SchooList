0a6741479350efa8403d9f16dcdd2de4c85a9a0f
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-01-26 12:20:16 -0300

    Test revert index

--- Android/app/src/main/java/br/com/poo/vinicius/scholist/ProvasTabletActivity.java
+++ Android/app/src/main/java/br/com/poo/vinicius/scholist/ProvasTabletActivity.java
@@ -2,8 +2,9 @@ package br.com.poo.vinicius.scholist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.R;
 
-public class ProvasTabletActivity extends AppCompatActivity {
+public class ProvasTabletActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
