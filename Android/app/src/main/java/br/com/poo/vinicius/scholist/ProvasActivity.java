cf78ea070976e9c3cfd69612e89e5f5d94e42a65
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-01-27 21:50:39 -0300

    Set fragment and set landscape, with bool value to verify state of app

--- Android/app/src/main/java/br/com/poo/vinicius/scholist/ProvasActivity.java
+++ Android/app/src/main/java/br/com/poo/vinicius/scholist/ProvasActivity.java
@@ -20,12 +20,46 @@ public class ProvasActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_lista_provas);
+        setContentView(R.layout.activity_provas);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+
         transaction.replace(R.id.frame_principal, new ListaProvasFragment());
+
+        if(estaNoModoPaisagem()) {
+            transaction.replace(R.id.frame_secundario, new DetalhesProvaFragment());
+        }
         transaction.commit();
 
     }
+
+    private boolean estaNoModoPaisagem() {
+        return getResources().getBoolean(R.bool.modoPaisagem);
+    }
+
+
+    public void selecionaProva(Prova prova) {
+        FragmentManager manager = getSupportFragmentManager();
+        if (!estaNoModoPaisagem()) {
+            FragmentTransaction tx = manager.beginTransaction();
+
+            DetalhesProvaFragment detalhesFragment = new DetalhesProvaFragment();
+            Bundle parametros = new Bundle();
+            parametros.putSerializable("prova", prova);
+            detalhesFragment.setArguments(parametros);
+
+            tx.replace(R.id.frame_principal, detalhesFragment);
+            tx.addToBackStack(null);
+
+            tx.commit();
+        } else {
+            DetalhesProvaFragment detalhesFragment =
+                    (DetalhesProvaFragment) manager.findFragmentById(R.id.frame_secundario);
+            detalhesFragment.populaCamposCom(prova);
+        }
+    }
+
+
 }
