540d19d037fe553154f30c06add9a9bf3ebf6027
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-02-01 15:23:06 -0300

    Add things in register

--- Android/app/src/main/java/br/com/poo/vinicius/scholist/RegisterActivity.java
+++ Android/app/src/main/java/br/com/poo/vinicius/scholist/RegisterActivity.java
@@ -1,7 +1,10 @@
 package br.com.poo.vinicius.scholist;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -23,6 +26,15 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import br.com.poo.vinicius.scholist.model.User;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -36,9 +48,6 @@ public class RegisterActivity extends AppCompatActivity {
     private Uri mSelecetedUri;
     private ImageView mImgPhoto;
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +59,7 @@ public class RegisterActivity extends AppCompatActivity {
         mUsername = findViewById(R.id.edit_username);
         mBtnImage = findViewById(R.id.btn_selected_photo);
         mEditUsername = findViewById(R.id.edit_username);
+        mImgPhoto = findViewById(R.id.img_photo);
 
 
         mBtnEnter.setOnClickListener(new View.OnClickListener() {
@@ -71,7 +81,29 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 0) {
+            mSelecetedUri = data.getData();
+
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), mSelecetedUri);
+                mImgPhoto.setImageDrawable(new BitmapDrawable(bitmap));
+                mBtnImage.setAlpha(0);
+            } catch(IOException e) {
+
+            }
+        }
+
     }
+
+
+    public void selectPhoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, 0);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -124,6 +156,53 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void saveUserInFirebase() {
+        String fileName = UUID.randomUUID().toString();
+        final StorageReference ref = FirebaseStorage.getInstance().getReference("/images/" + fileName);
+        ref.putFile(mSelecetedUri).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.i("Error", e.getMessage() );
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                         Log.i("uri",uri.toString());
+
+                         String uid = FirebaseAuth.getInstance().getUid();
+                         String nome = mEditUsername.getText().toString();
+                         String profileUri = uri.toString();
+                         User user = new User(uid, nome, profileUri);
+
+                         FirebaseFirestore.getInstance().collection("users")
+                                 .document(uid)
+                                 .set(user)
+                                 .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                     @Override
+                                     public void onSuccess(Void aVoid) {
+                                         Intent intent = new Intent(RegisterActivity.this, ListaAlunosActivity.class);
+                                         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                         startActivity(intent);
+                                     }
+                                 })
+                                 .addOnFailureListener(new OnFailureListener() {
+                                     @Override
+                                     public void onFailure(@NonNull Exception e) {
+                                         Log.i("Error", e.getMessage());
+                                     }
+                                 });
+
+                    }
+                });
+
+
+            }
+        });
+
+
+
     }
 
 
---
+++ Android/app/src/main/java/br/com/poo/vinicius/scholist/model/User.java
@@ -0,0 +1,67 @@
+package br.com.poo.vinicius.scholist.model;
+
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class User implements Parcelable {
+
+    private String uuid;
+    private String username;
+    private String profileUrl;
+
+    public User() {
+
+    }
+
+
+    public User(String uuid, String username, String profileUrl) {
+        this.uuid = uuid;
+        this.username = username;
+        this.profileUrl = profileUrl;
+    }
+
+
+    protected User(Parcel in) {
+        uuid = in.readString();
+        username = in.readString();
+        profileUrl = in.readString();
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(uuid);
+        dest.writeString(username);
+        dest.writeString(profileUrl);
+    }
+}
+
