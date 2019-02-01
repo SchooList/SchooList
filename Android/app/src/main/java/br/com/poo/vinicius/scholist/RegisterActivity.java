cdbed099d61552821ad184ce39a82c502c37e79c
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-02-01 13:57:22 -0300

    Add new listener

--- Android/app/src/main/java/br/com/poo/vinicius/scholist/RegisterActivity.java
+++ Android/app/src/main/java/br/com/poo/vinicius/scholist/RegisterActivity.java
@@ -1,19 +1,66 @@
 package br.com.poo.vinicius.scholist;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+
+    private EditText mEditUsername;
+    private EditText mEditEmail;
+    private EditText mEditPassword;
+    private Button mBtnEnter;
+    private EditText mUsername;
+    private Button mBtnImage;
+    private Uri mSelecetedUri;
+    private ImageView mImgPhoto;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        mEditEmail = findViewById(R.id.edit_email);
+        mEditPassword = findViewById(R.id.edit_password);
+        mBtnEnter = findViewById(R.id.btn_enter);
+        mUsername = findViewById(R.id.edit_username);
+        mBtnImage = findViewById(R.id.btn_selected_photo);
+        mEditUsername = findViewById(R.id.edit_username);
+
+
+        mBtnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        mBtnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -36,4 +83,25 @@ public class RegisterActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
 
     }
+
+    public void createUser() {
+        String email = mEditEmail.getText().toString();
+        String senha = mEditPassword.getText().toString();
+        String nome = mUsername.getText().toString();
+
+
+        if(email.isEmpty() || senha.isEmpty() || senha.isEmpty() || email == null || senha == null) {
+            Toast.makeText(RegisterActivity.this, "Os campos devem ser preenchidos", Toast.LENGTH_SHORT).show();
+
+            FirebaseAuth
+
+
+        }
+
+
+    }
+
+
+
+
 }
