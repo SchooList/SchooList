0cb7435ee59e99fe84c9bb04462709719efde689
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-02-01 20:29:53 -0300

    Implemented auth and login, but when i will create user thats ok, but crashed when intent to screen

--- Android/app/src/main/java/br/com/poo/vinicius/scholist/LoginActivity.java
+++ Android/app/src/main/java/br/com/poo/vinicius/scholist/LoginActivity.java
@@ -1,11 +1,25 @@
 package br.com.poo.vinicius.scholist;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import org.w3c.dom.Text;
 
@@ -16,15 +30,41 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+
+        final EditText mEditEmail = findViewById(R.id.edit_email);
+        final EditText mEditPassword = findViewById(R.id.edit_password);
+
+
         Button btnEnter = (Button) findViewById(R.id.btn_enter);
         TextView txtAccount = (TextView) findViewById(R.id.txt_account);
+
 
         btnEnter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, ListaAlunosActivity.class);
-                startActivity(intent);
-                finish();
+                String email = mEditEmail.getText().toString();
+                String senha = mEditPassword.getText().toString();
+
+                if(email == null || email.isEmpty() || senha.isEmpty() || senha == null) {
+                    Toast.makeText(LoginActivity.this, "Nome e Senha devem ser preenchidos", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.i("Sucesso", task.getResult().getUser().getUid());
+                        Intent intent = new Intent(LoginActivity.this, ListaAlunosActivity.class);
+                        startActivity(intent);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i("Error",e.getMessage().toString());
+                        return;
+                    }
+                });
             }
         });
 
--- Android/app/src/main/java/br/com/poo/vinicius/scholist/RegisterActivity.java
+++ Android/app/src/main/java/br/com/poo/vinicius/scholist/RegisterActivity.java
@@ -10,39 +10,38 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
+import java.time.Clock;
 import java.util.UUID;
+
 
 import br.com.poo.vinicius.scholist.model.User;
 
 public class RegisterActivity extends AppCompatActivity {
 
-
     private EditText mEditUsername;
     private EditText mEditEmail;
     private EditText mEditPassword;
-    private Button mBtnEnter;
+    private Button btnEnter;
     private EditText mUsername;
     private Button mBtnImage;
     private Uri mSelecetedUri;
@@ -55,29 +54,26 @@ public class RegisterActivity extends AppCompatActivity {
 
         mEditEmail = findViewById(R.id.edit_email);
         mEditPassword = findViewById(R.id.edit_password);
-        mBtnEnter = findViewById(R.id.btn_enter);
+        btnEnter = (Button) findViewById(R.id.btn_enter);
         mUsername = findViewById(R.id.edit_username);
         mBtnImage = findViewById(R.id.btn_selected_photo);
-        mEditUsername = findViewById(R.id.edit_username);
         mImgPhoto = findViewById(R.id.img_photo);
+        mEditUsername = findViewById(R.id.edit_username);
 
-
-        mBtnEnter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
+       btnEnter.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               createUser();
+           }
+       });
 
         mBtnImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                selectPhoto();
             }
         });
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -93,75 +89,48 @@ public class RegisterActivity extends AppCompatActivity {
             } catch(IOException e) {
 
             }
+
         }
 
     }
-
-
     public void selectPhoto() {
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
         startActivityForResult(intent, 0);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_register, menu);
-
-        return true;
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId())  {
-            case R.id.menu_right:
-                Intent intent = new Intent(RegisterActivity.this, ListaAlunosActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-
-    }
-
     public void createUser() {
         String email = mEditEmail.getText().toString();
         String senha = mEditPassword.getText().toString();
-        String nome = mUsername.getText().toString();
-
-
-        if(email.isEmpty() || senha.isEmpty() || senha.isEmpty() || email == null || senha == null) {
-            Toast.makeText(RegisterActivity.this, "Os campos devem ser preenchidos", Toast.LENGTH_SHORT).show();
+        String nome = mEditUsername.getText().toString();
+        if(nome == null || nome.isEmpty() || email == null || email.isEmpty() || senha.isEmpty() || senha == null) {
+            Toast.makeText(this, "Nome, Senha e E-mail devem ser preenchidas",Toast.LENGTH_SHORT).show();
+            return;
         }
 
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(task.isSuccessful()) {
-                        Toast.makeText(RegisterActivity.this, "Login is Successful", Toast.LENGTH_SHORT).show();
-                        saveUserInFirebase();
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Log.i("Sucesso", task.getResult().getUser().getUid());
+                            saveUserInFirebase();
+                        }
                     }
-            }
-        }).addOnFailureListener(new OnFailureListener() {
+                }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Log.i("Error", e.getMessage());
-
+                Log.i("Error",e.getMessage().toString());
             }
         });
-
-
     }
 
     private void saveUserInFirebase() {
         String fileName = UUID.randomUUID().toString();
-        final StorageReference ref = FirebaseStorage.getInstance().getReference("/images/" + fileName);
+        final StorageReference ref  = FirebaseStorage.getInstance().getReference("/images/" + fileName);
         ref.putFile(mSelecetedUri).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Log.i("Error", e.getMessage() );
+                Log.i("Teste",e.getMessage());
             }
         }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
             @Override
@@ -169,40 +138,35 @@ public class RegisterActivity extends AppCompatActivity {
                 ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                     @Override
                     public void onSuccess(Uri uri) {
-                         Log.i("uri",uri.toString());
+                        Log.i("teste",uri.toString());
+                        String uid = FirebaseAuth.getInstance().getUid();
+                        String nome = mEditUsername.getText().toString();
+                        String profileUri = uri.toString();
+                        User user = new User(uid,nome, profileUri);
 
-                         String uid = FirebaseAuth.getInstance().getUid();
-                         String nome = mEditUsername.getText().toString();
-                         String profileUri = uri.toString();
-                         User user = new User(uid, nome, profileUri);
-
-                         FirebaseFirestore.getInstance().collection("users")
-                                 .document(uid)
-                                 .set(user)
-                                 .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                     @Override
-                                     public void onSuccess(Void aVoid) {
-                                         Intent intent = new Intent(RegisterActivity.this, ListaAlunosActivity.class);
-                                         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                                         startActivity(intent);
-                                     }
-                                 })
-                                 .addOnFailureListener(new OnFailureListener() {
-                                     @Override
-                                     public void onFailure(@NonNull Exception e) {
-                                         Log.i("Error", e.getMessage());
-                                     }
-                                 });
+                        FirebaseFirestore.getInstance().collection("/users")
+                                .document(uid)
+                                .set(user)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Toast.makeText(RegisterActivity.this, "RÉ", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(RegisterActivity.this, ListaAlunosActivity.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.i("teste", e.getMessage());
+                                    }
+                                });
 
                     }
                 });
-
-
             }
         });
-
-
-
     }
 
 
