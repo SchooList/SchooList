3d8bc44a871b8ede6919743cd2906abf7b734893
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-02-23 22:46:28 -0300

    Add turma screen

---
+++ Android/app/src/main/java/br/com/poo/vinicius/scholist/TurmasActivity.java
@@ -0,0 +1,117 @@
+package br.com.poo.vinicius.scholist;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Picasso;
+import com.xwray.groupie.GroupAdapter;
+import com.xwray.groupie.Item;
+import com.xwray.groupie.OnItemClickListener;
+import com.xwray.groupie.ViewHolder;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import br.com.poo.vinicius.scholist.model.Turma;
+import br.com.poo.vinicius.scholist.model.User;
+
+public class TurmasActivity extends AppCompatActivity {
+
+    GroupAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_turmas);
+
+        RecyclerView rv = findViewById(R.id.recycler);
+
+        adapter = new GroupAdapter<>();
+        rv.setAdapter(adapter);
+
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(@NonNull Item item, @NonNull View view) {
+                Intent intent = new Intent(TurmasActivity.this, ListaAlunosActivity.class);
+                TurmaItem turmaItem = (TurmaItem)item;
+                intent.putExtra("turma",turmaItem.turma);
+                startActivity(intent);
+            }
+        });
+
+        fetchTurmas();
+
+
+    }
+
+    private void fetchTurmas() {
+        FirebaseFirestore.getInstance().collection("/turmas")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                        if(e != null) {
+                            Log.e("Teste", e.getMessage());
+                            return;
+                        }
+                        List<DocumentSnapshot> docs = queryDocumentSnapshots.getDocuments();
+                        for (DocumentSnapshot doc: docs) {
+                            Turma turma = doc.toObject(Turma.class);
+                            adapter.add(new TurmaItem(turma));
+                        }
+                        
+                    }
+                });
+
+
+    }
+
+
+    private class TurmaItem extends Item<ViewHolder> {
+
+        private final Turma turma;
+
+        private TurmaItem(Turma turma) {
+            this.turma = turma;
+        }
+
+        @Override
+        public void bind(@NonNull ViewHolder viewHolder, int position) {
+            TextView txtTurmaNome = viewHolder.itemView.findViewById(R.id.turma_textViewNome);
+            TextView txtTurmaDescricao = viewHolder.itemView.findViewById(R.id.turma_textViewDescricao);
+            ImageView imgPhoto = viewHolder.itemView.findViewById(R.id.turma_imageView);
+            txtTurmaNome.setText(turma.getNome());
+            txtTurmaDescricao.setText(turma.getDescricao());
+            Picasso.get()
+                    .load(turma.getProfileUrl())
+                    .into(imgPhoto);
+        }
+        @Override
+        public int getLayout() {
+            return R.layout.item_turma;
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Android/app/src/main/java/br/com/poo/vinicius/scholist/model/Turma.java
@@ -0,0 +1,100 @@
+package br.com.poo.vinicius.scholist.model;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.io.Serializable;
+
+public class Turma implements Parcelable {
+
+    String nome;
+    String descricao;
+    String profileUrl;
+    String uuid;
+    User users[];
+
+    public Turma() {
+
+    }
+
+    public Turma(String nome, String descricao, String profileUrl){
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    protected Turma(Parcel in) {
+        uuid = in.readString();
+        nome = in.readString();
+        descricao = in.readString();
+        profileUrl = in.readString();
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    public void profileUrl(String getProfileUrl) {
+        this.profileUrl = getProfileUrl;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public User[] getUsers() {
+        return users;
+    }
+
+    public void setUsers(User[] users) {
+        this.users = users;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Turma> CREATOR = new Creator<Turma>() {
+        @Override
+        public Turma createFromParcel(Parcel in) {
+            return new Turma(in);
+        }
+
+        @Override
+        public Turma[] newArray(int size) {
+            return new Turma[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(uuid);
+        dest.writeString(nome);
+        dest.writeString(descricao);
+        dest.writeString(profileUrl);
+    }
+}
