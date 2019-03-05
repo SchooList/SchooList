b300336bc9db2af231987d3f7fae486ff784d9ed
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-03-05 16:14:12 -0300

    Implemented firebase delete

--- Android/app/src/main/java/br/com/poo/vinicius/scholist/ConfiguracoesTurmaActivity.java
+++ Android/app/src/main/java/br/com/poo/vinicius/scholist/ConfiguracoesTurmaActivity.java
@@ -3,10 +3,13 @@ package br.com.poo.vinicius.scholist;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
@@ -40,7 +43,40 @@ public class ConfiguracoesTurmaActivity extends AppCompatActivity {
             btnSair.setText("Deletar turma");
         }
 
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(FirebaseAuth.getInstance().getUid().equals(turma.getUuidAdmin())) {
+                    deletarTurma();
+                } else {
+                    sairDaTurma();
+                }
+            }
+        });
+    }
 
+    private void sairDaTurma() {
+        FirebaseFirestore.getInstance().collection("users").document(FirebaseAuth.getInstance().getUid()).collection("turmas")
+                .document(turma.getUuid()).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Intent intent = new Intent(ConfiguracoesTurmaActivity.this, TurmasActivity.class);
+                startActivity(intent);
+                Toast.makeText(ConfiguracoesTurmaActivity.this, "Turma deletada com sucesso", Toast.LENGTH_LONG).show();
+            }
+        });
 
+    }
+
+    private void deletarTurma() {
+        FirebaseFirestore.getInstance().collection("turmas").document(turma.getUuid()).delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Intent intent = new Intent(ConfiguracoesTurmaActivity.this, TurmasActivity.class);
+                        startActivity(intent);
+                        Toast.makeText(ConfiguracoesTurmaActivity.this, "Turma deletada", Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
