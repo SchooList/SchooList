d9584aead25e0e339e68712261117694690187d4
Jonh1036 <jonh1036@hotmail.com>
2019-03-05 09:45:13 -0300

    Fixed bug fromId

--- Android/app/src/main/java/br/com/poo/vinicius/scholist/ChatActivity.java
+++ Android/app/src/main/java/br/com/poo/vinicius/scholist/ChatActivity.java
@@ -170,7 +170,7 @@ public class ChatActivity extends AppCompatActivity {
 
         @Override
         public int getLayout() {
-            return message.getFromId() == FirebaseAuth.getInstance().getUid() ? R.layout.item_from_message : R.layout.item_to_message;
+            return message.getFromId().equals(FirebaseAuth.getInstance().getUid()) ? R.layout.item_from_message : R.layout.item_to_message;
         }
     }
 
