14c80beb7abf1fbce121565e104a9dabe45b39de
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-03-05 17:21:46 -0300

    Implemented update turma, but i will fixed bugs

--- Android/app/src/main/java/br/com/poo/vinicius/scholist/ConfiguracoesTurmaActivity.java
+++ Android/app/src/main/java/br/com/poo/vinicius/scholist/ConfiguracoesTurmaActivity.java
@@ -1,18 +1,34 @@
 package br.com.poo.vinicius.scholist;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.util.UUID;
 
 import br.com.poo.vinicius.scholist.model.Turma;
 
@@ -23,6 +39,8 @@ public class ConfiguracoesTurmaActivity extends AppCompatActivity {
     ImageView imgTurma;
     EditText txtDescricaoTurma;
     Button btnSair;
+    Button btnSelectedPhoto;
+    private Uri mSelectedUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +50,7 @@ public class ConfiguracoesTurmaActivity extends AppCompatActivity {
         txtDescricaoTurma = findViewById(R.id.settings_turma_descricao);
         imgTurma = findViewById(R.id.settings_turma_imageView);
         btnSair = findViewById(R.id.settings_turma_sair);
+        btnSelectedPhoto = findViewById(R.id.settings_turma_botao_foto);
         Intent intent = getIntent();
         turma = intent.getParcelableExtra("turma");
 
@@ -43,6 +62,14 @@ public class ConfiguracoesTurmaActivity extends AppCompatActivity {
             btnSair.setText("Deletar turma");
         }
 
+
+        btnSelectedPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectPhoto();
+            }
+        });
+
         btnSair.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,6 +80,66 @@ public class ConfiguracoesTurmaActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void selectPhoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+            getMenuInflater().inflate(R.menu.menu_profile, menu);
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menu_ok:
+                updateTurma();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void updateTurma() {
+        Toast.makeText(ConfiguracoesTurmaActivity.this, "CHAMOU", Toast.LENGTH_LONG).show();
+        final String nomeTurma = txtNomeTurma.getText().toString();
+        final String descricaoTurma = txtDescricaoTurma.getText().toString();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final DocumentReference ref = db.collection("turmas").document(turma.getUuid());
+
+
+        String filename = UUID.randomUUID().toString();
+
+        ref.update("nome", nomeTurma);
+        ref.update("descricao", descricaoTurma);
+
+        final StorageReference ref1 = FirebaseStorage.getInstance().getReference("/images/" + filename);
+        ref1.putFile(mSelectedUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                ref1.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        String profileUrl = uri.toString();
+                        ref.update("profileUrl", profileUrl);
+
+                    }
+                });
+            }
+        });
+
+
+
+
+
     }
 
     private void sairDaTurma() {
@@ -79,4 +166,20 @@ public class ConfiguracoesTurmaActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 0) {
+            mSelectedUri = data.getData();
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), mSelectedUri);
+                imgTurma.setImageDrawable(new BitmapDrawable(bitmap));
+            } catch (IOException e) {
+            }
+        }
+    }
+
+
 }
